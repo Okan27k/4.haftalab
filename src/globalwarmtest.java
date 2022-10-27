b9dbe411c88f67a42dccf9b00da283dae523844d
aidata <aidata@10.121.3.198>
2022-10-27 12:20:48 +0300

    Global Warming Test

--- src/globalwarmtest.java
+++ src/globalwarmtest.java
@@ -5,47 +5,47 @@ public class globalwarmtest {
 		
 	static void soru1() {
 		System.out.println("Hangi ülke en çok sera gazý yaymaktadýr");
-		System.out.println("1: Çin");
-		System.out.println("2: Hindistan");
-		System.out.println("3: Amerika");
-		System.out.println("4: Japonya");
+		System.out.println("A: Çin");
+		System.out.println("B: Hindistan");
+		System.out.println("C: Amerika");
+		System.out.println("D: Japonya");
 	}
 		static void soru2() {
 		System.out.println("Hangi Gezegenin atmosferi küresel ýsýnma ile yok edilmektedir");
-		System.out.println("1: Jupiter");
-		System.out.println("2: Venüs");
-		System.out.println("3: Mars");
-		System.out.println("4:Merkür");
+		System.out.println("A: Jupiter");
+		System.out.println("B: Venüs");
+		System.out.println("C: Mars");
+		System.out.println("D:Merkür");
 	}
 		static void soru3() {
 		System.out.println("Aþaðýdakilerden hangisi küresel ýsýnmanýn olumsuz bir etkisi deðildir?");
-		System.out.println("1: Daha büyük balýk nüfusu.");
-		System.out.println("2: Yeni bulaþýcý hastalýklarýn ortaya çýkmasý.");
-		System.out.println("3: Türlerin yok olmasý.");
-		System.out.println("4: Kýyý alanlarýnýn kaybý.");
+		System.out.println("A: Daha büyük balýk nüfusu.");
+		System.out.println("B: Yeni bulaþýcý hastalýklarýn ortaya çýkmasý.");
+		System.out.println("C: Türlerin yok olmasý.");
+		System.out.println("D: Kýyý alanlarýnýn kaybý.");
 	}
 		static void soru4() {
 		System.out.println("Kirlilik havaya yayýldýkça ne azalýr?");
-		System.out.println("1: Okyanuslar.");
-		System.out.println("2: Ozon Tabakasý.");
-		System.out.println("3: Yaðmurlar.");
-		System.out.println("4: Karbondioksit.");	
+		System.out.println("A: Okyanuslar.");
+		System.out.println("B: Ozon Tabakasý.");
+		System.out.println("C: Yaðmurlar.");
+		System.out.println("D: Karbondioksit.");	
 	}
 		static void soru5() {
 		System.out.println(" Bunlardan hangisi toksik bir sera gazýdýr?");
-		System.out.println("1: Nitrojen.");
-		System.out.println("2: Oksijen.");
-		System.out.println("3: Karbonmonoksit.");
-		System.out.println("4: Metan.");
+		System.out.println("A: Nitrojen.");
+		System.out.println("B: Oksijen.");
+		System.out.println("C: Karbonmonoksit.");
+		System.out.println("D: Metan.");
 	}
 		
 	public static void main(String[]args){
 		Scanner s=new Scanner(System.in);
 		int sayac=0;
-		int[] dizi = new int[5];
+		String[] dizi = new String[5];
 		soru1();
-		dizi[0]= s.nextInt();
-		if(dizi[0]==1) {
+		dizi[0]= s.nextLine();
+		if(dizi[0].equals("a") || dizi[0].equals("A")) {
 			sayac++;
 			System.out.println("Doðru");
 			
@@ -54,8 +54,8 @@ public class globalwarmtest {
 		}
 		soru2();
 		
-		dizi[1]=s.nextInt();
-		if(dizi[1]==2) {
+		dizi[1]=s.nextLine();
+		if(dizi[1].equals("b")  || dizi[1].equals("B")) {
 			sayac++;
 			System.out.println("Doðru");
 			
@@ -63,8 +63,8 @@ public class globalwarmtest {
 			System.out.println("Yanlýþ");
 		}
 		soru3();
-		dizi[2]=s.nextInt();
-		if(dizi[2]==1) {
+		dizi[2]=s.nextLine();
+		if(dizi[2].equals("a")  || dizi[2].equals("A")) {
 			sayac++;
 			System.out.println("Doðru");
 		}else {
@@ -72,16 +72,16 @@ public class globalwarmtest {
 		}
 		
 		soru4();
-		dizi[3]=s.nextInt();
-		if(dizi[3]==2) {
+		dizi[3]=s.nextLine();
+		if(dizi[3].equals("b")  || dizi[3].equals("B")) {
 			sayac++;
 			System.out.println("Doðru");
 		}else {
 			System.out.println("Yanlýþ");
 		}
 		soru5();
-		dizi[4]=s.nextInt();
-		if(dizi[4]==4) {
+		dizi[4]=s.nextLine();
+		if(dizi[4].equals("d")  || dizi[1].equals("D")) {
 			sayac++;
 			System.out.println("Doðru");
 		}else {
